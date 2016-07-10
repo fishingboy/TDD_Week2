cf1b2723ca9af3f79a9c80380c1df72720e3b470
Leo.Kuo <et282523@hotmail.com>
2016-07-10 20:49:57 +0800

    綠燈 - 一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -136,4 +136,30 @@ class CartTest extends PHPUnit_Framework_TestCase
         // assert
         $this->assertEquals($expected, $actual);
     }
+
+    public function test_buy_bookNo1_quantity_is_1_and_BookNo2_quantity_is_1_and_BookNo3_quantity_is_2_total_should_be_375()
+    {
+        // arrange
+        $target = new Cart();
+        
+        $product = ['no' => 1, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+
+        $product = ['no' => 2, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+        
+        $product = ['no' => 3, 'price' => 100];
+        $quantity = 2;
+        $target->addProduct($product, $quantity);
+        
+        $expected = 370;
+
+        // act
+        $actual = $target->calculateTotal();
+
+        // assert
+        $this->assertEquals($expected, $actual);
+    }
 }
