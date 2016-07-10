57fb077087508579d5291b6c55b183cc1bb31073
Leo.Kuo <et282523@hotmail.com>
2016-07-10 17:27:04 +0800

    紅燈 - 第一集買一本，第二集買一本，總共 190 元

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -24,4 +24,26 @@ class CartTest extends PHPUnit_Framework_TestCase
         // assert
         $this->assertEquals($expected, $actual);
     }
+
+    public function test_buy_bookNo1_quantity_is_1_and_BookNo2_quantity_is_1_total_should_be_190()
+    {
+        // arrange
+        $target = new Cart();
+        
+        $product = ['no' => 1, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+
+        $product = ['no' => 2, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+        
+        $expected = 190;
+
+        // act
+        $actual = $target->calculateTotal();
+
+        // assert
+        $this->assertEquals($expected, $actual);
+    }
 }
