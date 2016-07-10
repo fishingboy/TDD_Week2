daaea48f5e13928e166791b54cd220f3b6c8679a
Leo.Kuo <et282523@hotmail.com>
2016-07-10 20:45:11 +0800

    紅燈 - 一次買了整套，一二三四五集各買了一本，價格應為100*5*0.75=375

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -102,4 +102,38 @@ class CartTest extends PHPUnit_Framework_TestCase
         // assert
         $this->assertEquals($expected, $actual);
     }
+
+    public function test_buy_bookNo1_quantity_is_1_and_BookNo2_quantity_is_1_and_BookNo3_quantity_is_1_and_BookNo4_quantity_is_1_and_BookNo5_quantity_is_1_total_should_be_375()
+    {
+        // arrange
+        $target = new Cart();
+        
+        $product = ['no' => 1, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+
+        $product = ['no' => 2, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+        
+        $product = ['no' => 3, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+
+        $product = ['no' => 4, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+        
+        $product = ['no' => 5, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+        
+        $expected = 375;
+
+        // act
+        $actual = $target->calculateTotal();
+
+        // assert
+        $this->assertEquals($expected, $actual);
+    }
 }
