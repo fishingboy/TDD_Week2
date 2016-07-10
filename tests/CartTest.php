28cd48800fb1375c51ff254bc1606c6d6283bea8
Leo.Kuo <et282523@hotmail.com>
2016-07-10 17:04:21 +0800

    紅燈 - 買第一集一本共 100 元

---
+++ tests/CartTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 購物車測試
+ */
+class CartTest extends PHPUnit_Framework_TestCase
+{
+    public function __construct()
+    {
+        xdebug_disable();
+    }
+
+    public function test_buy_bookNo1_quantity_is_1_total_should_be_100()
+    {
+        // arrange
+        $target = new Cart();
+        $product = ['no' => 1, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+        $expected = 100;
+
+        // act
+        $actual = $target->calculateTotal();
+
+        // assert
+        $this->assertEquals($expected, $actual);
+    }
+}
