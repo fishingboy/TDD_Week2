5ea88d909fa00b0058a8b88812c5e0126feb867b
Leo.Kuo <et282523@hotmail.com>
2016-07-10 20:42:09 +0800

    紅置 - 一二三四集各買了一本，價格應為100*4*0.8=320

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -72,4 +72,34 @@ class CartTest extends PHPUnit_Framework_TestCase
         // assert
         $this->assertEquals($expected, $actual);
     }
+
+    public function test_buy_bookNo1_quantity_is_1_and_BookNo2_quantity_is_1_and_BookNo3_quantity_is_1_and_BookNo4_quantity_is_1_total_should_be_320()
+    {
+        // arrange
+        $target = new Cart();
+        
+        $product = ['no' => 1, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+
+        $product = ['no' => 2, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+        
+        $product = ['no' => 3, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+
+        $product = ['no' => 4, 'price' => 100];
+        $quantity = 1;
+        $target->addProduct($product, $quantity);
+        
+        $expected = 320;
+
+        // act
+        $actual = $target->calculateTotal();
+
+        // assert
+        $this->assertEquals($expected, $actual);
+    }
 }
